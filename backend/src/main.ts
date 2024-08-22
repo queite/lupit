64c10744ce74ad1f1f487e34e2a6c46acf296e03
Queite <queitesc@gmail.com>
2024-08-21 21:27:47 -0300

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe)
-
+  app.enableCors()
+  
   await app.listen(3333);
 }
 bootstrap();
