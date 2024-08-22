97e45d6b2b8f68014f081529077eeab43165af7e
Queite <queitesc@gmail.com>
2024-08-21 22:25:35 -0300

    style: add style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,14 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div>
+  <main>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+      <div className="container">
         <Header title="FutLovers"/>
+        <div className="home-title">
+          <h1>Jogadores</h1>
+          <button>+   ADICIONAR JOGADOR</button>
+        </div>
         <PlayerTable/>
       </div>
     </main>
